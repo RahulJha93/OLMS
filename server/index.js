78c1ae483b0e752f380585235c16677264277bd2
Rahul Jha <rahul.sahyogcollege@gmail.com>
2025-05-14 05:42:03 +0530

    fix : backend url

--- server/index.js
+++ server/index.js
@@ -22,7 +22,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:["http://localhost:5173","https://olms-major.vercel.app"],
     credentials:true
 }));
  
