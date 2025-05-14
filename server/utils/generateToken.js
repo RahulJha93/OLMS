cda2b7310b9ba8b622c0bd3dd459ae173cd287f3
Rahul Jha <rahul.sahyogcollege@gmail.com>
2025-05-14 06:07:03 +0530

    cookies reoslve

--- server/utils/generateToken.js
+++ server/utils/generateToken.js
@@ -9,9 +9,10 @@ export const generateToken = (res, user, message) => {
     .status(200)
     .cookie("token", token, {
       httpOnly: true,
-      sameSite: "strict",
-      secure: true,
+      sameSite: "none", // 'none' for prod, 'lax' for dev
+      secure: true, // true for prod, false for dev
       maxAge: 24 * 60 * 60 * 1000, // 1 day
+      // domain: process.env.NODE_ENV === "production" ? ".yourdomain.com" : undefined, // Uncomment and set for subdomain usage
     }).json({
         success:true,
         message,
