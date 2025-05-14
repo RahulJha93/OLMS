5e784c4e16334ee8c14a2fe5134e062d152fe88e
Rahul Jha <rahul.sahyogcollege@gmail.com>
2025-05-14 06:16:29 +0530

    fix : vercel spa

--- server/utils/generateToken.js
+++ server/utils/generateToken.js
@@ -9,8 +9,8 @@ export const generateToken = (res, user, message) => {
     .status(200)
     .cookie("token", token, {
       httpOnly: true,
-      sameSite: "none", // 'none' for prod, 'lax' for dev
-      secure: true, // true for prod, false for dev
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax", // 'none' for prod, 'lax' for dev
+      secure: process.env.NODE_ENV === "production", // true for prod, false for dev
       maxAge: 24 * 60 * 60 * 1000, // 1 day
       // domain: process.env.NODE_ENV === "production" ? ".yourdomain.com" : undefined, // Uncomment and set for subdomain usage
     }).json({
